adb14365dd6791e69289f162e7d559a919bba45c
ahmed-abdelwahed1 <ahmedbinshehata@gmail.com>
2025-05-30 18:55:23 +0300

    edit timer

--- scripts.js
+++ scripts.js
@@ -117,7 +117,7 @@ document.addEventListener('DOMContentLoaded', function () {
         async function startTypingEffect() {
             while (true) {
                 await typeText(heroTitle, originalText, 100);
-                await new Promise(resolve => setTimeout(resolve, 7000)); // Wait 7 seconds
+                await new Promise(resolve => setTimeout(resolve, 6000)); // Wait 6 seconds
                 heroTitle.style.animation = 'none';
                 heroTitle.offsetHeight; // Trigger reflow
                 heroTitle.style.animation = 'typing 3.5s steps(40, end), blink-caret 0.75s step-end infinite';
